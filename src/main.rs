7212416f8a185a0ca8432d3df209be8aa6bdafc7
Bryant Mairs <bryant@mai.rs>
2016-04-17 22:31:17 -0700

    Add serial initialization.

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,65 @@
 
 #![crate_type = "bin"]
 
+extern crate argparse;
 extern crate gtk;
+extern crate serial;
 
+use std::process;
+
+use argparse::{ArgumentParser, Store};
 use gtk::prelude::*;
+use serial::BaudRate;
+use serial::prelude::*;
+
+enum ExitCode {
+    ArgumentError = 1,
+    BadPort,
+    ConfigurationError
+}
 
 fn main() {
+    // Store command-line arguments
+    let mut serial_port_name = "".to_string();
+    let mut serial_baud = "115200".to_string();
+
+    // Parse command-line arguments
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("A serial terminal.");
+        ap.refer(&mut serial_port_name)
+            .add_option(&["-p", "--port"], Store, "The serial port name (COM3, /dev/ttyUSB0, etc.)")
+            .required();
+        ap.refer(&mut serial_baud)
+            .add_option(&["-b", "--baud"], Store, "The serial port baud rate (default 115200)");
+        ap.parse_args_or_exit();
+    }
+
+    // Convert arguments to numbers
+    let baud : usize = match serial_baud.parse() {
+        Ok(t) => t,
+        Err(_) => { println!("Improper serial baud rate specified ({})", serial_baud); process::exit(ExitCode::ArgumentError as i32)}
+    };
+
+    // Open the specified serial port
+    let mut port = match serial::open(&serial_port_name) {
+        Ok(m) => { m }
+        Err(e) => { println!("Failed to open {}: {}", serial_port_name, e.to_string()); process::exit(ExitCode::BadPort as i32)}
+    };
+
+    // Configure the port settings
+    match port.reconfigure(&|settings| {
+        try!(settings.set_baud_rate(BaudRate::from_speed(baud)));
+        settings.set_char_size(serial::Bits8);
+        settings.set_parity(serial::ParityNone);
+        settings.set_stop_bits(serial::Stop1);
+        settings.set_flow_control(serial::FlowNone);
+        Ok(())
+    }) {
+        Ok(_) => (),
+        Err(e) => { println!("Failed to configure {}: {}", serial_port_name, e.to_string()); process::exit(ExitCode::ConfigurationError as i32)}
+    }
+
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
         return;
