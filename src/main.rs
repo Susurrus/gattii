97bf384831594c32465a0562f4b92d701583b926
Bryant Mairs <bryant@mai.rs>
2016-04-18 21:59:40 -0700

    Start the basics of the 0.1 GUI.
    Dropdown menu of serial ports and a play button.
    A textfield to display text on.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-//! # Basic Sample
-//!
-//! This sample demonstrates how to create a toplevel `window`, set its title, size and position, how to add a `button` to this `window` and how to connect signals with actions.
-
 #![crate_type = "bin"]
 
 extern crate argparse;
 extern crate gtk;
 extern crate serial;
 
+use std::io::prelude::*;
+use std::fs::File;
+use std::io::BufReader;
 use std::process;
 
 use argparse::{ArgumentParser, Store};
@@ -68,24 +67,66 @@ fn main() {
         return;
     }
 
+    // Create the main window
     let window = gtk::Window::new(gtk::WindowType::Toplevel);
-
-    window.set_title("First GTK+ Program");
-    window.set_border_width(10);
+    window.set_title("Gattii - Your Serial Terminal Interface");
     window.set_position(gtk::WindowPosition::Center);
-    window.set_default_size(350, 70);
+    window.set_default_size(400, 300);
+
+    // Create a toolbar with the basic options in it
+    let toolbar = gtk::Toolbar::new();
+    let ports_selector = gtk::ComboBoxText::new();
+    ports_selector.append(None, "/dev/ttyUSB0");
+    ports_selector.set_active(0);
+    let ports_selector_container = gtk::ToolItem::new();
+    ports_selector_container.add(&ports_selector);
+    toolbar.add(&ports_selector_container);
+    let open_icon = gtk::Image::new_from_icon_name("media-playback-start",
+                                                   gtk::IconSize::SmallToolbar as i32);
+    let open_button = gtk::ToolButton::new::<gtk::Image>(Some(&open_icon), None);
+    open_button.set_is_important(true);
+    toolbar.add(&open_button);
+
+    // Set up an auto-scrolling text view
+    let text_view = gtk::TextView::new();
+    text_view.set_editable(false);
+    let scroll = gtk::ScrolledWindow::new(None, None);
+    scroll.set_policy(gtk::PolicyType::Automatic, gtk::PolicyType::Automatic);
+    scroll.add(&text_view);
+
+    // Pack everything vertically
+    let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
+    vbox.pack_start(&toolbar, false, true, 0);
+    vbox.pack_start(&scroll, true, true, 0);
+    window.add(&vbox);
+
+    let window1 = window.clone();
+    open_button.connect_clicked(move |_| {
+        // TODO move this to a impl?
+        let file_chooser = gtk::FileChooserDialog::new(
+            Some("Open File"), Some(&window1), gtk::FileChooserAction::Open);
+        file_chooser.add_buttons(&[
+            ("Open", gtk::ResponseType::Ok as i32),
+            ("Cancel", gtk::ResponseType::Cancel as i32),
+        ]);
+        if file_chooser.run() == gtk::ResponseType::Ok as i32 {
+            let filename = file_chooser.get_filename().unwrap();
+            let file = File::open(&filename).unwrap();
+
+            let mut reader = BufReader::new(file);
+            let mut contents = String::new();
+            let _ = reader.read_to_string(&mut contents);
+
+            text_view.get_buffer().unwrap().set_text(&contents);
+        }
+
+        file_chooser.destroy();
+    });
 
     window.connect_delete_event(|_, _| {
         gtk::main_quit();
         Inhibit(false)
     });
-
-    let button = gtk::Button::new_with_label("Click me!");
-
-    button.connect_clicked(|_| {
-        println!("Clicked!");
-    });
-    window.add(&button);
 
     window.show_all();
     gtk::main();
