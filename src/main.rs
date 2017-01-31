e310289a5df8c38375528b8d54bdba07884bb833
Bryant Mairs <bryant@mai.rs>
2017-01-31 10:58:37 -0800

    Disable unsupported features in crates.io's gtk

--- src/main.rs
+++ src/main.rs
@@ -151,7 +151,8 @@ fn main() {
     port_settings_button.set_direction(gtk::ArrowType::None);
     let port_settings_popover = gtk::Popover::new(Some(&port_settings_button));
     port_settings_popover.set_position(gtk::PositionType::Bottom);
-    port_settings_popover.set_constrain_to(gtk::PopoverConstraint::None);
+    // TODO: Once gtk > 0.1.1 is released uncomment the following line
+    //port_settings_popover.set_constrain_to(gtk::PopoverConstraint::None);
     port_settings_button.set_popover(Some(&port_settings_popover));
     let popover_container = gtk::Grid::new();
     popover_container.set_margin_top(10);
